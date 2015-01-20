e8d92a4ba3b0323df4b71c64ee71ef3a7fac1b80
stellarak <stellarak@gmail.com>
2015-01-20 07:02:30 +0200

    Still trying

---
+++ script/Connection.js
@@ -0,0 +1,52 @@
+ // Wait for device API libraries to load
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+
+		function onDeviceReady() {
+
+	//Every 5sec check if there is a connection
+			setInterval(function(){
+					//If there is no connection there is an alert
+			if (navigator.connection.type == Connection.NONE)
+				{
+				 alert('Please Connect!' );
+
+				}
+			else
+					//If there is connection call checkConnection
+				{
+					 checkConnection();
+				 }
+
+			},5000);
+		}
+
+
+        function checkConnection() {
+
+        		//The message depends on the type of the connection
+            var State = navigator.connection.type;
+
+			if (State == Connection.ETHERNET)
+			{
+				console.log("Device is connected to Ethernet");
+			}
+			else if(State == Connection.WIFI)
+			{
+				console.log("Device is connected to WiFi");
+			}
+			else if(State == Connection.CELL_2G)
+			{
+				console.log("Device is connected to 2G");
+			}
+			else if(State == Connection.CELL_3G)
+			{
+				console.log("Device is connected to 3G");
+			}
+			else if(State == Connection.CELL4G)
+			{
+				console.log("Device is connected to 4G");
+			}
+		}
